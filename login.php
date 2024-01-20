1ef0453d6588685fa542f166a84b9d70a7013645
pullaposki <pullaposki@windowslive.com>
2024-01-20 11:32:24 +0200

    Initial commit

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($_POST)) {
+    echo "post not found";
+    return;
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username === 'admin' && $password === 'password') {
+    echo json_encode(['status' => 'success']);
+} else {
+    echo json_encode(['status' => 'error']);
+}
+?>
