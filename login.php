944dd744060adf88dc5a0fc1ab0973f75852eafe
pullaposki <pullaposki@windowslive.com>
2024-01-26 12:29:47 +0200

    Add session start and database connection

--- login.php
+++ login.php
@@ -1,7 +1,14 @@
 <?php
+session_start();
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+include './db/DbConnector.php';
+
+$connector = new DbConnector();
+$connection = $connector->connect();
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
@@ -13,8 +20,29 @@ if (!isset($_POST)) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username === 'admin' && $password === 'password') {
+$stmt = $connection->prepare('SELECT * FROM employees WHERE username = ?');
+$stmt->bind_param('s', $username);
+$stmt->execute();
+$result = $stmt->get_result()->fetch_assoc();
+
+if (passwordOK($password, $result) && is_array($result)) {
+    $_SESSION['user_id'] = $result['id'];
     echo json_encode(['status' => 'success']);
 } else {
     echo json_encode(['status' => 'error']);
+}
+
+
+// if ($username === 'admin' && $password === 'password') {
+//     echo json_encode(['status' => 'success']);
+// } else {
+//     echo json_encode(['status' => 'error']);
+// }
+
+function passwordOK($result, $password): bool
+{
+    if ($result && password_verify($password, $result['password']))
+        return true;
+
+    return false;
 }
